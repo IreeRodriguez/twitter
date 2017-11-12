217d5ed7e0d7110962470669b7fedafe265f9d22
IreeRodriguez <ire208@gmail.com>
2017-11-12 11:44:22 -0300

    text area cambiando de tamano

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,15 @@
+
 var field = 0;
 /*Funcion para contador que me dara siempre el ultimo valor del length del texto*/
 var limitText = function (limitField, limitCount, limitNum) {
+
+    var textA = document.getElementsByName("message")[0];
+
+
     limitCount.value = limitNum - limitField.value.length;
     field = limitField.value.length;
+    textA.style.height = "25px";
+    textA.style.height = (20+textA.scrollHeight)+"px";
 };
 
 var twit = function (fieldValue) {
@@ -12,86 +19,29 @@ var twit = function (fieldValue) {
         var sendButton = document.getElementsByName("enviar")[0];
         var thinkInput = document.getElementsByName("message")[0];
         var timeLine = document.getElementsByName("lineaDeTiempo")[0];
-        document.getElementById("textInput").value="";
 
-        sendButton.onclick = function() {
+
+        // sendButton.onclick = function() {
 
             var tuitDiv = document.createElement("div");
-            var nameSpan = document.createElement("span");
             var dateSpan = document.createElement("span");
             var tuitP = document.createElement("p");
-            tuitDiv.appendChild(nameSpan);
-            tuitDiv.appendChild(dateSpan);
+            var date = new Date()
             tuitDiv.appendChild(tuitP);
+            tuitDiv.appendChild(dateSpan);
             tuitDiv.className = "tuit";
-            nameSpan.textContent = "El Brayatan";
-            dateSpan.textContent = new Date();
+            dateSpan.textContent = date.toLocaleTimeString();
             tuitP.textContent = thinkInput.value;
+
+            timeLine.style.display="block";
+
             timeLine.insertBefore(tuitDiv, timeLine.children[0]);
+
+            document.getElementById("textInput").value="";
+            field=0;
+            document.getElementsByName("countdown")[0].value="140";
+
+        } else {
+            return false;
         }
     }
-}
-
-
-            //     if(limitField.value.length>0 && limitField.value.length<=140){
-              //
-            //         var sendButton = document.getElementsByName("enviar")[0];
-            //         var thinkInput = document.getElementsByName("message")[0];
-            //         var timeLine = document.getElementsByName("lineaDeTiempo")[0];
-              //
-            //         sendButton.onclick = function() {
-              //
-            //             var tuitDiv = document.createElement("div");
-            //             var nameSpan = document.createElement("span");
-            //             var dateSpan = document.createElement("span");
-            //             var tuitP = document.createElement("p");
-            //             tuitDiv.appendChild(nameSpan);
-            //             tuitDiv.appendChild(dateSpan);
-            //             tuitDiv.appendChild(tuitP);
-            //             tuitDiv.className = "tuit"
-            //             nameSpan.textContent = "El Brayatan";
-            //             dateSpan.textContent = new Date();
-            //             tuitP.textContent = thinkInput.value;
-            //             timeLine.insertBefore(tuitDiv, timeLine.children[0]);
-            //         }
-            //     }
-            //   }
-
-
-// window.onload = function() {
-//
-//
-//
-//     /*Esto es porque soy flojo, getElements..entrega arreglo y
-//     solo queremos el primero de sus elementos que retorna porque
-//     sabemos que existe al menos uno*/
-//     var sendButton = document.getElementsByName("enviar")[0];
-//     var thinkInput = document.getElementsByName("message")[0];
-//     var timeLine = document.getElementsByName("lineaDeTiempo")[0];
-//     sendButton.onclick = function() {
-//         if (thinkInput.value == "") {
-//             alert("tení k poner algo oe!");
-//         } else {
-//             var tuitDiv = document.createElement("div");
-//             var nameSpan = document.createElement("span");
-//             var dateSpan = document.createElement("span");
-//             var tuitP = document.createElement("p");
-//             tuitDiv.appendChild(nameSpan);
-//             tuitDiv.appendChild(dateSpan);
-//             tuitDiv.appendChild(tuitP);
-//             tuitDiv.className = "tuit"
-//             nameSpan.textContent = "El Brayatan";
-//             dateSpan.textContent = new Date();
-//             tuitP.textContent = thinkInput.value;
-//
-//             /* Usamos timeLine.children para obtener los hijos
-//              del nodo, este siempre es un arreglo, aunque venga
-//              vacío.
-//              YYYYYYYY... como insertBefore recibe dos parámetros y
-//              el segundo es opcional, si, el primer nodo no existe en
-//              el arreglo children, entonces nos dará undefined. Y eso hará
-//              que insertBefore agregue el nodo al último */
-//             timeLine.insertBefore(tuitDiv, timeLine.children[0]);
-//         }
-//     }
-// }
